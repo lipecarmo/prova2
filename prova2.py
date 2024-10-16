a59147b931d54ffa8325ed5a6778b5ac8b53e24d
lipecarmo <lipeoliveira523@gmail.com>
2024-10-15 23:01:29 -0300

    Add files via upload

---
+++ prova2.py
@@ -0,0 +1,14 @@
+
+numero = float(input("Digite um número: "))
+
+
+if numero > 0:
+    resultado = "O número é positivo."
+elif numero < 0:
+    resultado = "O número é negativo."
+else:
+    resultado = "O número é zero."
+
+
+
+print(f"{resultado}")
